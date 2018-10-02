69de418995c27c38db25141423512fbf929a74ae
tod.shen <tod.shen@104.com.tw>
2018-10-02 17:17:04 +0800

    ignore test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  return true
-});
+it('renders without crashing', () => { });
